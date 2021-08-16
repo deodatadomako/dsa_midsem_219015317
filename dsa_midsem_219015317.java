34476592525e0bb861f8034a02ec9606a5f1b67f
Deodat <deodatadomako@gmail.com>
2021-08-16 18:24:13 +0100

    initial commit

---
+++ dsa_midsem_219015317.java
@@ -0,0 +1,35 @@
+import java.util. Random;
+
+class Main
+
+{
+
+public static void remove(int[] arr) Random rand = new Random(); int n = arr.length; 
+
+{
+
+until the length of array bec while(n> 0) {
+
+System.out.println(
+
+removing element, i.e for (int i=pos; i<n-1; i+
+
+{
+
+}
+
+arr[i] = arr[i+1
+
+decresing the n to no n--;
+
+generating next posit if(n > 0) pos= rand.nextI
+
+}
+
+}
+
+public static void main(String[] args) { int[] arr = {1, 2, 3, 4, 5}; remove (arr);
+
+}
+
+}
